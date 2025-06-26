ec5d1706f0fbd0609a7b8e9a26fb77b6d40a54f7
markm <markmutisya72@gmail.com>
2025-06-26 07:29:17 -0700

    Initial Com

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,5 +1,7 @@
-import { Redirect, Stack } from 'expo-router';
+import { Redirect, Tabs } from 'expo-router';
 import {useAuth} from "@clerk/clerk-expo";
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '../../constants/colors';
 
 const TabsLayout = () => {
   const { isSignedIn } = useAuth()
@@ -8,8 +10,45 @@ const TabsLayout = () => {
     return <Redirect href={'/auth/sign-in'} />
   };
   return (
-    <Stack />
+    <Tabs 
+    screenOptions={{
+      headerShown: false,
+      tabBarActiveTintColor: COLORS.primary,
+      tabBarInactiveTintColor: COLORS.textLight,
+      tabBarStyle: {
+        backgroundColor: COLORS.white,
+        borderTopColor: COLORS.border,
+        borderTopWidth: 1,
+        paddingBottom: 8,
+        paddingTop: 8,
+        height: 80
+      },
+      tabBarLabelStyle: {
+        fontSize: 12,
+        fontWeight: "600",
+      }
+    }}>
+      <Tabs.Screen name="index"
+      options={{
+        title:"Recipe",
+        tabBarIcon: ({color, size}) => <Ionicons name="restaurant" size={size} color={color} />,
+      }}/>
+      
+      <Tabs.Screen name="search"
+      options={{
+        title:"Search",
+        tabBarIcon: ({color, size}) => <Ionicons name="search" size={size} color={color} />, 
+      }}/>
+      
+      <Tabs.Screen name="favorites"
+      options={{
+        title:"Favorites",
+        tabBarIcon: ({color, size}) => <Ionicons name="heart" size={size} color={color} />,
+         
+      }}/>
+
+    </Tabs>
   )
-}
+};
 
 export default TabsLayout;
