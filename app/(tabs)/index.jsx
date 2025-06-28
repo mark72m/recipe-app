75aed9af83416a75c23d262d181e90671f727b9c
markm <markmutisya72@gmail.com>
2025-06-28 06:19:53 -0700

    Index Editing

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,7 +1,62 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import { useState } from 'react';
+import { useRouter} from 'expo-router';
+import { MealAPI} from "../../services/mealAPI";
 
 const HomeScreen = () => {
+  const router = useRouter();
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [recipes, setRecipes] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [featuredRecipe, setFeaturedRecipe] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const loadData = async () => {
+    try {
+      setLoading(true);
+      const [apiCategories, randomMeals, featuredMeal] = await Promise.all([
+        MealAPI.getCategories(),
+        MealAPI.getRandomMeals(12),
+        MealAPI.getRandomMeal(),
+      ]);
+
+      const transformedCategories = apiCategories.map((cat, index) => ({
+        id: index + 1,
+        name: cat.strCategory,
+        image: cat.strCategoryThumb,
+        description: cat.strCategoryDescription,
+      }));
+
+      setCategories(transformedCategories);
+
+      const transformedMeals = randomMeals
+      .map((meal) => MealAPI.transformMealData(meal))
+      .filter((meal) => meal !== null)
+
+      setRecipes(transformedMeals);
+
+      const transformedFeatured = MealAPI.transformMealData(featuredMeal);
+      setFeaturedRecipe(transformedFeatured);
+    } catch(error) {
+      console.log("Error Loading the Data", error)
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const loadCategoryData = async (category) => {
+    try {
+      const meals = await MealAPI.filterByCategory(category);
+      const transformedMeals = meals
+      .map((meal) => MealAPI.transformMealData(meal))
+      .filter((meal) => meal !== null)
+      setRecipes(transformedMeals);
+    } catch (error) {
+      console.error("Error Loading Category Data:", error);
+      setRecipes([]);
+    }
+  }
   return (
     <View>
       <Text>HomeScreen</Text>
