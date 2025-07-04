aa3753d440a2da3d4e73bc73390ce81642056c4f
markm <markmutisya72@gmail.com>
2025-07-04 04:29:08 -0700

    Recipe with Video

---
+++ recipe/[id].jsx
@@ -0,0 +1,60 @@
+import { View, Text } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { useUser } from '@clerk/clerk-expo';
+import { API_URL } from '../constants/api';
+import {MealAPI} from '../services/mealAPI';
+
+const RecipeDetailsScreen = () => {
+    const {id:recipeId} = useLocalSearchParams();
+    const [recipe, setRecipe] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [isSaved, setIsSaved] = useState(false);
+    const [isSaving, setIsSaving] = useState(false);
+
+    const {user} = useUser();
+    const userId = user?.id;
+
+
+    useEffect(() => {
+        const checkIfSaved = async () => {
+            try {
+                const response = await fetch(`${API_URL}/favorites/${userId}`);
+                const favorites = await response.json();
+                const isRecipeSaved = favorites.some((fav) => fav.recipeId === parseInt (recipeId));
+                setIsSaved(isRecipeSaved);
+            } catch (error) {
+                console.error("Error Checking if Recipe is Saved:", error);
+            }
+        };
+
+        const loadRecipeDetail = async () => {
+            setLoading(true);
+            try {
+                const mealData = await MealAPI.getMealById(recipeId);
+                if(mealData) {
+                    const transformedRecipe = MealAPI.transformMealData(mealData);
+
+                    const recipeWithVideo = {
+                        ...transformedRecipe,
+                        youtubeUrl: mealData.strYoutube || null,
+                    };
+
+                    setRecipe(recipeWithVideo);
+                }
+
+            } catch (error){
+
+            } finally {
+                setLoading(false);
+            }
+        }
+    }, [])
+  return (
+    <View>
+      <Text>RecipeDetailsScreen</Text>
+    </View>
+  )
+}
+
+export default RecipeDetailsScreen
